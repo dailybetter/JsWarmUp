c49875ab762d5e20f9e4f9caaf64601d42773df6
dailybetter <a40293865@gmail.com>
2022-12-16 01:54:34 +0900

    js100-40

--- JS100/38.js
+++ JS100/38.js
@@ -5,4 +5,21 @@
 1위 ~ 3위 학생은 여러명일 수 있고 1~3위 학생 중 중복되는 학생까지 포함하여 사탕을 사기로 한다.
 **학생들의 점수를 공백으로 구분하여 입력을 받고 사탕을 받을 학생의 수를 출력하세요.** */
 
-const li = prompt('점수를 입력하세요').split(' ').map((n) => parseInt(n,10))
+const score_li = prompt('점수를 입력하세요').split(' ').map((n) => parseInt(n,10))
+
+score_li.sort((a,b) => {
+    return a-b;
+});
+
+let count = 0;
+let arr = [];
+
+while (arr.length < 3) {
+  let n = scores.pop();
+  if (!arr.includes(n)){
+    arr.push(n);
+  }
+  count += 1;
+}
+
+console.log(count);
