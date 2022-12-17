3785e1efdc59f29b6234f6bbebf3ff7cec1da05a
dailybetter <a40293865@gmail.com>
2022-12-17 17:47:19 +0900

    js100-42

---
+++ JS100/41-42.js
@@ -0,0 +1,40 @@
+/* # 문제41 : 소수판별
+
+숫자가 주어지면 **소수인지 아닌지 판별하는 프로그램**을 작성해주세요.
+소수이면 YES로, 소수가 아니면 NO로 출력해주세요.
+(소수 : 1과 자기 자신만으로 나누어떨어지는 1보다 큰 양의 정수) */
+
+const isPrimte(num){
+    if(num>1){
+        let count = 0
+        for(let i= 2; i < num; i++){
+            if(num%i ==0){
+                count += 1
+            }
+        }
+        if(count == 0){
+            console.log('YES')
+        }else{
+            console.log('NO')
+        }
+    }
+}
+
+/* # 문제42 : 2020년
+
+2020년 1월 1일은 수요일입니다. 2020년 a월 b일은 무슨 요일일까요?
+두 수 a, b를 입력받아 2020년 a월 b일이 무슨 요일인지 리턴하는 함수 solution을 완성하세요.
+요일의 이름은 일요일부터 토요일까지 각각 SUN, MON, TUE, WED, THU, FRI, SAT 입니다.
+
+예를 들어 a = 5, b = 24라면 5월 24일은 일요일이므로 문자열 "SUN"를 반환하세요. */
+
+const month = prompt('월을 입력하세요.');
+const date = prompt('일을 입력하세요.');
+
+function solution(a,b){
+    const day = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+
+    const x = new Date('2020-'+a+'-'+b);
+    return day[x.getDay()];
+}
+console.log(solution(month, date));
